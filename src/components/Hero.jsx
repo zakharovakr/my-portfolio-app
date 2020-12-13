49b37dcc569ba73e1088933aab038a59e6b4deb6
Kristina Zakharova <zakharova.kr@gmail.com>
2020-12-12 21:55:10 -0500

    added border to my image in css
    + changed the description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ export default function Hero() {
   return (
     <div id='hero'>
       <div className="hero-img">
-        <img src={Me} alt="" />
+        <img src={Me} alt="my photo" />
         <h1 data-aos='fade-up' data-aos-delay="1500" data-aos-duration='1000'>Kristina Zakharova</h1>
-        <h2 data-aos='fade-up' data-aos-delay="2000" data-aos-duration='1500'>Junior Full-Stack Software Engineer</h2>
+        <h2 data-aos='fade-up' data-aos-delay="2000" data-aos-duration='1500'>Junior Full-Stack Developer</h2>
       </div>
     </div>
   )
