ee7d5606245b15a8ccc3e625c6c60e3535264ed3
Kristina Zakharova <zakharova.kr@gmail.com>
2020-12-12 21:20:28 -0500

    created a hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Me from '../images/me.jpeg';
+
+export default function Hero() {
+  return (
+    <div id='hero'>
+      <div className="hero-img">
+        <img src={Me} alt="" />
+        <h1 data-aos='fade-up' data-aos-delay="1500" data-aos-duration='1000'>Kristina Zakharova</h1>
+        <h2 data-aos='fade-up' data-aos-delay="2000" data-aos-duration='1500'>Junior Full-Stack Software Engineer</h2>
+      </div>
+    </div>
+  )
+}
